3affbc11dd0e957b242d27504c2cca7c4650ed18
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2024-05-21 10:44:46 +0530

    Update tailwind config to pickup changes from parent directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
 export default {
-  content: ['./theme/**/*.ftl'],
+  content: ['../**/*.ftl'],
   experimental: {
     optimizeUniversalDefaults: true,
   },
